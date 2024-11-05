e811eb18cee62d3319af7e2e460bc6abf52c93de
Isaac <122332664+IsaacGrosser@users.noreply.github.com>
2024-11-05 00:43:13 -0600

    Added larger background w/ added details. Created somewhat functional spikes. Added some folders for better organization

---
+++ Spikes.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var timer = $Timer
+var randGen = RandomNumberGenerator.new()
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		print("You have died!")
+		body.rotation = randGen.randf_range(-70, 70)
+		# gets player collision body and removes it so character falls through the floor when dying
+		body.get_node("CollisionShape2D").queue_free()
+		
+		# Sets time to go at half speed! Must reset it!
+		Engine.time_scale = 0.5
+		timer.start()
+
+func _on_timer_timeout():
+	# Sets time to go at default speed
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
+
